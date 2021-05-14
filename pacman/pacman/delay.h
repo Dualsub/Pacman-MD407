8ff81747aa44cea18cffe2cb300df45ec4fa1de9
filipwramdemark <filip.wramdemark@hotmail.com>
2021-05-14 11:37:28 +0200

    Fixed delay

---
+++ pacman/pacman/delay.h
@@ -0,0 +1,13 @@
+ // Definerar att simulatorn nu används.
+ #define SIMULATOR
+ 
+ // Definitioner för SysTick och dess register.
+ #define STK		0xE000E010
+ #define STK_CTRL 	((unsigned int*) STK)
+ #define STK_LOAD	((unsigned int*)(STK + 0x4))
+ #define STK_VAL	((unsigned int*)(STK + 0x8))
+ #define STK_CALIB	((unsigned int*)(STK + 0x0C))
+
+void delay_250ns();
+void delay_milli(unsigned int ms);
+void delay_mikro(unsigned int us);
