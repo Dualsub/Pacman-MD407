19c45ee158904bbc76af4a62a5e331cbcf6301c5
Dualsub <rizedualsub@gmail.com>
2021-05-14 14:37:30 +0200

    Added files for ghosts and pacman

--- pacman/pacman/object.h
+++ pacman/pacman/object.h
@@ -1,10 +1,12 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include "graphics.h"
+
 // Definerar ett objekt med geometri, position och riktning.
 typedef struct tObj 
 {
-	PGEOMERTY geo;
+	GEOMERTY* geo;
 	int dirx,diry;
 	int posx,posy;
 	void (* draw) (struct tObj *);
@@ -12,5 +14,17 @@ typedef struct tObj
 	void (* move) (struct tObj *);
 	void (* set_speed) (struct tObj *, int, int);
 } OBJECT, *POBJECT;
+
+#define WALL_SIZE 8
+
+typedef struct 
+{
+	int posx,posy;
+	unsigned char sizex, sizey;
+} WALL;
+
+void pacman_move(OBJECT* obj);
+void ghosts_move(OBJECT* obj);
+
 
 #endif
