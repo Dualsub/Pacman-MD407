74570be7312dda0ff20906e6e1098a3bb88f2013
filipwramdemark <filip.wramdemark@hotmail.com>
2021-05-19 10:01:54 +0200

    Added overlap functions to ghost.h

--- pacman/pacman/ghost.h
+++ pacman/pacman/ghost.h
@@ -8,5 +8,7 @@
 
 void ghost_move(OBJECT* obj, POINT* points, unsigned char num_points);
 void ghost_draw(OBJECT* obj);
+void ghost_wall_overlap(OBJECT* obj, POINT* points, unsigned char num_points);
+char ghost_pacman_collide(OBJECT* ghost, OBJECT* pacman);
 
 #endif
